8edd08a3f9502137cb0fe9fa7845202b5dc688b3
Sheepheerd <forordination@tutanota.com>
2024-08-11 18:17:17 -0400

    polybar versions

--- .screenlayout/monitor.sh
+++ .screenlayout/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-#xrandr --output DP-0 --primary --mode 1920x1080 --rate 143.98 --pos 0x0 --output DP-4 --mode 1920x1080 --rate 74.97 --pos 1920x-250 --output DP-2 --mode 1920x1080 --rate 75 --pos -1080x-710 --rotate right
+xrandr --output HDMI-0 --off --output DP-0 --primary --mode 1920x1080 --pos 1080x677 --rotate normal --rate 144 --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --mode 1920x1080 --pos 0x0 --rotate left --rate 75 --output DP-5 --off
--- config/polybar/laptop/README.md
+++
@@ -1,7 +0,0 @@
-# Polybar
-
-![Polybar](Polybar.png)
-
-## Description
-
-[Polybar](https://github.com/polybar/polybar) is a customizable and easy to use statusbar. It provides a more elegent than i3wm's i3bar and blocks.
