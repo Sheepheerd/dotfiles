2213ef6f002deff6a8d9519ab364acff41cb65e9
shepard <shepard>
2024-09-09 11:11:27 -0400

    Changed some binding for jdtls

--- .config/nvim/lua/custom/lsp/jdtls/keymaps.lua
+++ .config/nvim/lua/custom/lsp/jdtls/keymaps.lua
@@ -34,27 +34,65 @@ local vopts = {
 }
 
 local mappings = {
-	J = {
-		name = "Java",
-		o = { "<Cmd>lua require'jdtls'.organize_imports()<CR>", "Organize Imports" },
-		v = { "<Cmd>lua require('jdtls').extract_variable()<CR>", "Extract Variable" },
-		c = { "<Cmd>lua require('jdtls').extract_constant()<CR>", "Extract Constant" },
-		m = { "<Cmd>lua require('jdtls').extract_method(true)<CR>", "Extract Method" },
-
-		-- t = { "<Cmd>lua require'jdtls'.test_nearest_method()<CR>", "Test Method" },
-		-- T = { "<Cmd>lua require'jdtls'.test_class()<CR>", "Test Class" },
-		u = { "<Cmd>JdtUpdateConfig<CR>", "Update Config" },
+	{ "<leader>J", group = "Java", nowait = true, remap = false },
+	{
+		"<leader>Jc",
+		"<Cmd>lua require('jdtls').extract_constant()<CR>",
+		desc = "Extract Constant",
+		nowait = true,
+		remap = false,
+	},
+	{
+		"<leader>Jm",
+		"<Cmd>lua require('jdtls').extract_method(true)<CR>",
+		desc = "Extract Method",
+		nowait = true,
+		remap = false,
+	},
+	{
+		"<leader>Jo",
+		"<Cmd>lua require'jdtls'.organize_imports()<CR>",
+		desc = "Organize Imports",
+		nowait = true,
+		remap = false,
+	},
+	{ "<leader>Ju", "<Cmd>JdtUpdateConfig<CR>", desc = "Update Config", nowait = true, remap = false },
+	{
+		"<leader>Jv",
+		"<Cmd>lua require('jdtls').extract_variable()<CR>",
+		desc = "Extract Variable",
+		nowait = true,
+		remap = false,
 	},
 }
 
 local vmappings = {
-	J = {
-		name = "Java",
-		v = { "<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>", "Extract Variable" },
-		c = { "<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>", "Extract Constant" },
-		m = { "<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>", "Extract Method" },
+	{
+		mode = { "v" },
+		{ "<leader>J", group = "Java", nowait = true, remap = false },
+		{
+			"<leader>Jc",
+			"<Esc><Cmd>lua require('jdtls').extract_constant(true)<CR>",
+			desc = "Extract Constant",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>Jm",
+			"<Esc><Cmd>lua require('jdtls').extract_method(true)<CR>",
+			desc = "Extract Method",
+			nowait = true,
+			remap = false,
+		},
+		{
+			"<leader>Jv",
+			"<Esc><Cmd>lua require('jdtls').extract_variable(true)<CR>",
+			desc = "Extract Variable",
+			nowait = true,
+			remap = false,
+		},
 	},
 }
 
-which_key.register(mappings, opts)
-which_key.register(vmappings, vopts)
+which_key.add(mappings, opts)
+which_key.add(vmappings, vopts)
--- .config/nvim/lua/plugins/leap.lua
+++ .config/nvim/lua/plugins/leap.lua
@@ -6,6 +6,6 @@ return {
 	config = function()
 		vim.keymap.set({ "n", "x", "o" }, "<Tab>", "<Plug>(leap-forward)")
 		vim.keymap.set({ "n", "x", "o" }, "<S-Tab>", "<Plug>(leap-backward)")
-		vim.keymap.set({ "n", "x", "o" }, "<Tab>w", "<Plug>(leap-from-window)")
+		vim.keymap.set({ "n", "x", "o" }, "<leader><Tab>", "<Plug>(leap-from-window)")
 	end,
 }
