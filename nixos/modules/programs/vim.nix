98e4850daaa0496f1f0b7e28510095f21513bfeb
Sheepheerd <sheepheerd@novastar>
2024-11-14 23:39:28 -0500

    vim

--- nixos/modules/programs/vim.nix
+++ nixos/modules/programs/vim.nix
@@ -1,18 +1,15 @@
-{config, pkgs, inputs, lib, nixvim, ...}:
+{config, pkgs, inputs, lib, ...}:
 {
 
 imports = [ inputs.nixvim.nixvimModule ];
 
 environment.systemPackages = with pkgs; [
-  inputs.Neve.packages.${pkgs.system}.default
+  inputs.nixvim.packages.${pkgs.system}.default
 ];
 
 programs.nixvim = {
   enable = true;
-  imports = [ inputs.Neve.nixvimModule ];
 # Then configure Nixvim as usual, you might have to lib.mkForce some of the settings
-  colorschemes.catppuccin.enable = lib.mkForce false;
-  colorschemes.nord.enable = lib.mkForce false;
 };
 
 }
