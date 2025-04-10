0f00a0eddd3a747d4edd2267c799ba317dce1923
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-04-10 15:32:50 -0400

    fixing jdtls

--- nixos/home-manager/shared/nvim/plugins/languages/nvim-jdtls.nix
+++ nixos/home-manager/shared/nvim/plugins/languages/nvim-jdtls.nix
@@ -18,10 +18,10 @@ let
 
   allBundles = debugJarPaths ++ testJarPaths;
 
-  jdtlsFeaturesDir = "${pkgs.jdt-language-server}/share/java/jdtls/features";
+  jdtlsFeaturesDir = "${pkgs.jdt-language-server}/share/java/jdtls/plugins";
   featureFiles = builtins.attrNames (builtins.readDir jdtlsFeaturesDir);
   matchingFile = builtins.head (builtins.filter
-    (f: builtins.match "org.eclipse.equinox.executable_.*.jar" f != null)
+    (f: builtins.match "org.eclipse.equinox.launcher_1.*.jar" f != null)
     featureFiles);
   jarPath = "${jdtlsFeaturesDir}/${matchingFile}";
 in {
@@ -34,15 +34,28 @@ in {
     plugins.jdtls = {
       enable = true;
       settings = {
-        cmd = [
-          "java"
-          "-data"
-          "${config.home.homeDirectory}/.cache/jdtls/workspace"
-          "-jar"
-          jarPath
-          "-configuration"
-          "${pkgs.jdt-language-server}/share/java/jdtls/config_linux/config.ini"
-        ];
+        # cmd = [
+        #   "java"
+        #   "-Declipse.application=org.eclipse.jdt.ls.core.id1"
+        #   "-Dosgi.bundles.defaultStartLevel=4"
+        #   "-Declipse.product=org.eclipse.jdt.ls.core.product"
+        #   "-Dlog.protocol=true"
+        #   "-Dlog.level=ALL"
+        #   "-Xmx1g"
+        #   "--add-modules=ALL-SYSTEM"
+        #   "--add-opens"
+        #   "java.base/java.util=ALL-UNNAMED"
+        #   "--add-opens"
+        #   "java.base/java.lang=ALL-UNNAMED"
+        #   "-jar"
+        #   # jarPath
+        #   "${pkgs.jdt-language-server}/share/java/jdtls/plugins/org.eclipse.equinox.launcher_1.6.1000.v20250131-0606.jar"
+        #   "-configuration"
+        #   "${pkgs.jdt-language-server}/share/java/jdtls/config_linux"
+        #   "-data"
+        #   "${config.home.homeDirectory}/.cache/jdtls/workspace"
+        # ];
+        cmd = [ "${pkgs.jdt-language-server}/bin/jdtls" ];
         init_options = { bundles = allBundles; };
       };
     };
