b5757f1efcd0a0856929d72e2fac0823bd714134
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-07-19 20:47:23 -0400

    fix: sleep issue with nvidia

--- nixos/modules/nixos/common/boot.nix
+++ nixos/modules/nixos/common/boot.nix
@@ -23,5 +23,15 @@
 
       consoleLogLevel = 3;
     };
+    systemd = {
+      targets = {
+        sleep.enable = false;
+        suspend.enable = false;
+        hibernate.enable = false;
+        hybrid-sleep.enable = false;
+
+      };
+
+    };
   };
 }
