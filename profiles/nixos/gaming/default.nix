8e907e5f494b7f8e1e26c1756ac206d4af35e4ee
Shepard Garrett <shepard@heerd.dev>
2025-10-01 09:22:30 -0400

    fix: wrong modules before modules

--- profiles/nixos/gaming/default.nix
+++ profiles/nixos/gaming/default.nix
@@ -3,10 +3,10 @@
   options.solarsystem.profiles.gaming = lib.mkEnableOption "is this a gaming host";
   config = lib.mkIf config.solarsystem.profiles.gaming {
     solarsystem.modules = {
-      modules.sunshine = lib.mkDefault true;
-      modules.steam = lib.mkDefault true;
-      modules.minecraft = lib.mkDefault true;
-      modules.dolphin = lib.mkDefault true;
+      sunshine = lib.mkDefault true;
+      steam = lib.mkDefault true;
+      minecraft = lib.mkDefault true;
+      dolphin = lib.mkDefault true;
     };
 
   };
