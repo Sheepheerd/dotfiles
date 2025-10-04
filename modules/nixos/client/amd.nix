cd62933e04f4ade562b967533db44b7213297c4b
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-10-04 19:40:47 -0400

    fix: amdvlk

--- modules/nixos/client/amd.nix
+++ modules/nixos/client/amd.nix
@@ -18,10 +18,6 @@
         enable32Bit = true;
       };
 
-      amdgpu.amdvlk = {
-        enable = true;
-        support32Bit.enable = true;
-      };
     };
 
     boot.initrd.kernelModules = [ "amdgpu" ];
