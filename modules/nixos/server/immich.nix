d51084c9c238534b0466b6984397136bd6f00b92
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-12-03 17:18:09 -0500

    fix: postgress broke bruh

--- modules/nixos/server/immich.nix
+++ modules/nixos/server/immich.nix
@@ -14,8 +14,8 @@ in
   config = lib.mkIf config.solarsystem.modules.server.immich {
 
     users.users.${serviceUser} = {
-      home = "/var/lib/immich";
-      createHome = true;
+      # home = "/var/lib/immich";
+      # createHome = true;
       extraGroups = [
         "video"
         "render"
@@ -29,6 +29,17 @@ in
       host = "0.0.0.0";
       port = servicePort;
       mediaLocation = "/mnt/one-t-ssd/immich"; # dataDir
+
+      database = {
+        enable = true;
+        enableVectors = true;
+      };
+
+    };
+    services.postgresql = {
+      enable = true;
+      package = pkgs.postgresql_16;
+      dataDir = "/var/lib/postgresql/16";
     };
 
     # networking.firewall.allowedTCPPorts = [ 3001 ];
@@ -42,17 +53,13 @@ in
           locations = {
             "/" = {
               proxyPass = "http://localhost:3001";
+              proxyWebsockets = true;
+              recommendedProxySettings = true;
               extraConfig = ''
-                client_max_body_size    0;
-
-                proxy_http_version 1.1;
-                proxy_set_header   Upgrade    $http_upgrade;
-                proxy_set_header   Connection "upgrade";
-                proxy_redirect     off;
-
-                proxy_read_timeout 600s;
-                proxy_send_timeout 600s;
-                send_timeout       600s;
+                client_max_body_size 50000M;
+                proxy_read_timeout   600s;
+                proxy_send_timeout   600s;
+                send_timeout         600s;
               '';
             };
           };
