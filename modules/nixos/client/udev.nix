8ddb1903f8482211e6381895829ad0ee57719b33
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-12-16 11:09:49 -0500

    fix: bad udev

--- modules/nixos/client/udev.nix
+++ modules/nixos/client/udev.nix
@@ -13,12 +13,8 @@ in
 
   config = mkIf cfg.udev {
     services.udev.extraRules = ''
-      # TI-Nspire
-      SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="e012", ENV{ID_PDA}="1"
-      # TI-Nspire CX II
-      SUBSYSTEM=="usb", ATTR{idVendor}=="0451", ATTR{idProduct}=="e022", ENV{ID_PDA}="1"
 
-      SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", GROUP="plugdev, MODE="0666""
+      SUBSYSTEM=="usb", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="3000", GROUP="plugdev", MODE="0666"    
     '';
   };
 }
