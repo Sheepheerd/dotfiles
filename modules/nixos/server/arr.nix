8ecf6a5bc9afa9dfd0bd1182ab161203b7d92a47
Sheepheerd <130428152+Sheepheerd@users.noreply.github.com>
2025-12-19 00:41:42 -0500

    fix: removed microvm

--- modules/nixos/server/arr.nix
+++ modules/nixos/server/arr.nix
@@ -12,81 +12,6 @@ in
 
   config = lib.mkIf cfg {
 
-    # microvm.vms = {
-    #   my-microvm = {
-    #     config = {
-    #
-    #       microvm.shares = [
-    #         {
-    #           proto = "virtiofs";
-    #           tag = "agenix";
-    #           source = "/var/run/agenix.d/";
-    #           mountPoint = "/var/run/agenix";
-    #           readOnly = true;
-    #         }
-    #       ];
-    #       microvm.hypervisor = "qemu";
-    #       # Make VM reachable from host
-    #       microvm.interfaces = [
-    #         {
-    #           # NAT interface with ports forwarded
-    #           type = "user";
-    #           id = "enp4s0";
-    #           mac = "b4:2e:99:e9:97:33";
-    #           # hostPort:guestPort
-    #         }
-    #       ];
-    #       microvm.forwardPorts = [
-    #         {
-    #           from = "host";
-    #           host.port = 8112;
-    #           guest.port = 8112;
-    #         }
-    #         {
-    #           from = "host";
-    #           host.port = 2222;
-    #           guest.port = 22;
-    #         }
-    #       ];
-    #       imports = [
-    #         # inputs.nixarr.nixosModules.default
-    #       ];
-    #       users.users.root.initialPassword = "root";
-    #       networking.firewall.allowedTCPPorts = [
-    #         8112
-    #         22
-    #         58846
-    #       ];
-    #       services = {
-    #
-    #         openssh = {
-    #           enable = true;
-    #           settings = {
-    #             PermitRootLogin = "yes";
-    #           };
-    #         };
-    #         mullvad-vpn.enable = true;
-    #       };
-    #
-    #       services.resolved = {
-    #         enable = true;
-    #       };
-    #       services.deluge = {
-    #         enable = true;
-    #         web.enable = true;
-    #       };
-    #       services.deluge.dataDir = "/var/lib/deluge";
-    #     };
-    #   };
-    # };
-
-    # age.secrets = {
-    #   vpn-env-file = {
-    #     file = self + /secrets/server/arr/vpn.age;
-    #     symlink = false;
-    #     path = "/var/run/agenix.d/vpn-env-file";
-    #   };
-    # };
     nixarr = {
       enable = true;
       radarr = {
@@ -112,17 +37,17 @@ in
             };
           };
         };
-        "prowlarr.heerd.dev" = {
-          enableACME = true;
-          forceSSL = true;
-          acmeRoot = null;
-          locations = {
-            "/" = {
-              proxyPass = "http://127.0.0.1:9696";
-              extraConfig = '''';
-            };
-          };
-        };
+        # "prowlarr.heerd.dev" = {
+        #   enableACME = true;
+        #   forceSSL = true;
+        #   acmeRoot = null;
+        #   locations = {
+        #     "/" = {
+        #       proxyPass = "http://127.0.0.1:9696";
+        #       extraConfig = '''';
+        #     };
+        #   };
+        # };
         "deluge.heerd.dev" = {
           enableACME = true;
           forceSSL = true;
